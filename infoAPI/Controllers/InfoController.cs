5cc35cf371936d00e9a9e5e10103b09364a9c73e
Phuse <sasuk3san@gmail,com>
2022-06-09 20:40:47 +0100

    On branch master

---
+++ infoAPI/Controllers/InfoController.cs
@@ -0,0 +1,44 @@
+﻿using infoAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace infoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        static List<Info>_info = new List<Info>()
+        {
+            new Info()
+            {
+                 Id = 1,
+                 EmailAddress = "xyz.com",
+                 FirstName = "John",
+                 LastName = "Doe",
+                 DateOfBirth = "23-1-1990",
+                 ContactNumber = "070849302901",
+                 OfficeNumber = 6
+            }
+        };
+        public IEnumerable<Info> Get()
+        {
+            return _info;
+        }
+        [HttpPost]
+        public void Post([FromBody] Info info)
+        {
+            _info.Add(info);
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Info info)
+        {
+            _info[id] = info;
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _info.RemoveAt(id);
+        }
+    }
+}
---
+++ infoAPI/Models/Info.cs
@@ -0,0 +1,13 @@
+﻿namespace infoAPI.Models
+{
+    public class Info
+    {
+        public int Id { get; set; } 
+        public string EmailAddress { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string DateOfBirth { get; set; } = "";
+        public string ContactNumber { get; set; } = "";
+        public int OfficeNumber { get; set; } 
+    }
+}
